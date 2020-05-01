181a78c77a5b0698b185984be2723cf2bb672d3a
jasminluka <jasminluka007@gmail.com>
2020-05-01 23:56:51 +0200

    Created Bootcamp model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+require('colors');
 
 const connectDB = require('./config/db');
 
@@ -21,11 +22,11 @@ app.use('/api/v1/bootcamps', require('./routes/api/v1/bootcamps'));
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red.bold);
 
   // Close server & exit process
   server.close(() => process.exit(1));
