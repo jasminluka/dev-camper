08c072c2aa93fc4839e06aaacd875b9b8116ea85
jasminluka <jasminluka007@gmail.com>
2020-05-01 23:28:15 +0200

    Connected to database

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
+const connectDB = require('./config/db');
+
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -16,4 +21,12 @@ app.use('/api/v1/bootcamps', require('./routes/api/v1/bootcamps'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
