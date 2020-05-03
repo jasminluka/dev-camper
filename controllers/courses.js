296d2b1a7391e3ec1963b7526503d27c9d1313ec
jasminluka <jasminluka007@gmail.com>
2020-05-03 15:38:24 +0200

    Created a middleware for queries and pagination

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,25 +8,25 @@ const asyncHandler = require('../utils/async');
 // @desc    Get all courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
+  // For a specific bootcamp
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  }
-  else {
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description'
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+      return next(new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`, 404));
+    }
+
+    const courses = await Course.find({ bootcamp });
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
     });
   }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses
-  });
+  else {
+    res.status(200).json(res.advancedResults);
+  }
 });
 
 // @route   GET api/v1/courses/:id
--- routes/api/v1/bootcamps.js
+++ routes/api/v1/bootcamps.js
@@ -11,6 +11,9 @@ const {
   photoUploadBootcamp
 } = require('../../../controllers/bootcamps');
 
+const Bootcamp = require('../../../models/Bootcamp');
+const advancedResults = require('../../../middlewares/advancedResults'); 
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -19,7 +22,7 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router
   .route('/')
-  .get(getBootcamps)
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
   .post(createBootcamp);
 
 router
