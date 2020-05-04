ab7ed1af9251d2add059ceffda861f80369a7116
jasminluka <jasminluka007@gmail.com>
2020-05-04 14:42:34 +0200

    Bootcamp User Relationship and Ownership

--- seeder.js
+++ seeder.js
@@ -5,6 +5,7 @@ require('colors');
 
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -21,11 +22,15 @@ const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+
+
 // Import data into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log('Data Imported...'.green.inverse);
 
@@ -41,6 +46,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log('Data Destroyed...'.red.inverse);
 
